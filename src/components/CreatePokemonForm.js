b422d78393ab57389344a7199970ffc43f0a07d5
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-03-21 15:14:19 -0700

    init

--- src/components/CreatePokemonForm.js
+++ src/components/CreatePokemonForm.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { getPokemonTypes } from '../store/pokemon';
-
+  
 const CreatePokemonForm = ({ hideForm }) => {
   const pokeTypes = useSelector(state => state.pokemon.types);
   const dispatch = useDispatch();
